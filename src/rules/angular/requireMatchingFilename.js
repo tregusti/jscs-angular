4f81486f36f1c757d4c8834907a1de6cb782fe05
Glenn Jorde <glenn@tregusti.com>
2014-09-04 15:29:29 +0200

    Add requireMatchingFilename rule.

---
+++ src/rules/angular/requireMatchingFilename.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var format = require('util').format;
+var spah = require('spahql');
+var path = require('path');
+
+module.exports = function () {};
+
+module.exports.prototype.check = function check(file, errors) {
+  file.iterateNodesByType(['ExpressionStatement'], function(expression) {
+    var status = angularDefinitionName(expression);
+
+    if (status.valid) {
+      var filename = path.basename(file.getFilename());
+
+      if (filename === 'input') { return; }
+
+      if (status.name !== path.basename(filename, '.js')) {
+        var msg = 'Defined name \'%s\' is not matching the filename \'%s\'.';
+        errors.add(format(msg, status.name, filename), status.position);
+      }
+    }
+  });
+};
+module.exports.prototype.configure = function configure(value) {
+  this._value = value;
+};
+
+function angularDefinitionName(node) {
+  var data = spah.db(node);
+
+  if (!data.assert('/type=="ExpressionStatement"')) { return false; }
+
+  var expression = data.select('/expression');
+  if (!expression.assert('/type=="CallExpression"')) { return false; }
+
+  var allowed = '"controller", "service", "factory", "directive", "provider"';
+  var query = '/callee[/type=="MemberExpression"]/property[/type=="Identifier"][/name }<{ {%s}]';
+  if (!expression.assert(format(query, allowed))) { return false; }
+
+  var nameset = expression.select('/arguments[/.size==2]/0[/type=="Literal"]');
+  if (!nameset.length) { return false; }
+
+  return {
+    valid: true,
+    name: nameset.value().value,
+    position: nameset.value().loc.start
+  };
+}
